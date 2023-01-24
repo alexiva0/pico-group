b037e6368e2c22ea706f4231f49ff15f863889d8
fruktovoe-ragu <fruktovoe.ragu@gmail.com>
2023-01-24 21:21:09 +0200

    mobile h1, person tile

--- src/pages/people/Person/Person.tsx
+++ src/pages/people/Person/Person.tsx
@@ -13,45 +13,45 @@ const Person: React.FC<IPersonProps> = ({ data }) => {
 
   return (
     <div className={cn()}>
-      <div className={cn('container')}>
-        <div className={cn('image-box')}>
+      <div className={cn('image-box')}>
           {image ?
             <img src={image} className={cn('image')} alt="name" />
             : <div className={cn('no-image')} />
           }
         </div>
-        <div>
+      <div className={cn('container')}>
+        <div className={cn('name-box')}>
           <h5 className={cn('name')}>{name}</h5>
           {publications && (
             <a target="_blank" rel="noreferrer" href={publications} className={cn('publication')}>Research activity on{'\u00A0'}Pulbons.com</a>
           )}
         </div>
-      </div>
-      <div className={cn('info')}>
-        {address && (
-          <div className={cn('wrap')}>
-            <p className={cn('title')}>Workplace:</p>
-            <p className={cn('text')}>{address}</p>
-          </div>
-        )}
-        {phone && (
-          <div className={cn('wrap')}>
-            <p className={cn('title')}>Tel:</p>
-            <p dangerouslySetInnerHTML={{__html: phone}} />
-          </div>
-        )}
-        {fax && (
-          <div className={cn('wrap')}>
-            <p className={cn('title')}>Fax:</p>
-            <p className={cn('text')}>{fax}</p>
-          </div>
-        )}
-        {email && (
-          <div className={cn('wrap')}>
-            <p className={cn('title')}>Email:</p>
-            <a href={`mailto: ${email}`} className={cn('text')}>{email}</a>
-          </div>
-        )}
+        <div className={cn('info')}>
+          {address && (
+            <div className={cn('wrap')}>
+              <p className={cn('title')}>Workplace:</p>
+              <p className={cn('text')}>{address}</p>
+            </div>
+          )}
+          {phone && (
+            <div className={cn('wrap')}>
+              <p className={cn('title')}>Tel:</p>
+              <p dangerouslySetInnerHTML={{__html: phone}} />
+            </div>
+          )}
+          {fax && (
+            <div className={cn('wrap')}>
+              <p className={cn('title')}>Fax:</p>
+              <p className={cn('text')}>{fax}</p>
+            </div>
+          )}
+          {email && (
+            <div className={cn('wrap')}>
+              <p className={cn('title')}>Email:</p>
+              <a href={`mailto: ${email}`} className={cn('text')}>{email}</a>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
